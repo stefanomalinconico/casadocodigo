8bf2be0f10a058bb54284ebe39ae43596afa6b31
stefanomalinconico <stefanocmalinconico@gmail.com>
2016-01-29 15:31:23 -0200

    teste

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -1,15 +1,22 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.ProductDAO;
 import br.com.casadocodigo.loja.models.Product;
 
 @Controller
 public class ProductsController {
+	
+	@Autowired
+	private ProductDAO productDAO;
+	
 	@RequestMapping("/produtos")
 	public String save(Product product) {
 		System.out.println("Cadastrando o produto " + product);
+		productDAO.save(product);
 		return "products/ok";
 	}
 
