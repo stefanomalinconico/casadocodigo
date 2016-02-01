1d6b2150fd581db225e5649b86db4c6d0b6cac40
stefanomalinconico <stefanocmalinconico@gmail.com>
2016-02-01 12:07:55 -0200

    correções de teste

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -17,7 +17,7 @@ public class ProductsController {
 	public String save(Product product) {
 		System.out.println("Cadastrando o produto " + product);
 		productDAO.save(product);
-		return "products/ok";   jjjjjjjjjjj
+		return "products/ok";
 	}
 
 	@RequestMapping("/produtos/form")
--- casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
@@ -1,18 +1,19 @@
 package br.com.casadocodigo.loja.daos;
 
-import javax.persistence.PersistenceContext;
+
 
 import org.springframework.stereotype.Repository;
 
 import br.com.casadocodigo.loja.models.Product;
 
+
 @Repository
 public class ProductDAO {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	/*@PersistenceContext
+	private EntityManager manager;*/
 
 	public void save(Product product) {
-		manager.persist(product); gjhghg 
-	}m.,mm, lkjlkjk
+		//manager.persist(product); 
+	}
 }
